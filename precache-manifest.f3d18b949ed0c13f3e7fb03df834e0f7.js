c59ef6b22325e2203802335e9c116590f3578d09
mavandrade4 <mavandrade4@gmail.com>
2025-07-18 13:54:14 +0100

    Updates

---
+++ precache-manifest.f3d18b949ed0c13f3e7fb03df834e0f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a9aea23fade9d377bf47deb9c4633f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69e07aef81bd62ee7293",
+    "url": "/static/css/main.e9659d0f.chunk.css"
+  },
+  {
+    "revision": "848e884a7997c1d7068c",
+    "url": "/static/js/2.6c26b221.chunk.js"
+  },
+  {
+    "revision": "beec0e467c2ebf7fe3165ce50c9f165f",
+    "url": "/static/js/2.6c26b221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d51f9099ee6f9cb3977",
+    "url": "/static/js/3.fc4c893b.chunk.js"
+  },
+  {
+    "revision": "69e07aef81bd62ee7293",
+    "url": "/static/js/main.65545bc4.chunk.js"
+  },
+  {
+    "revision": "02a35d7026f72339b623",
+    "url": "/static/js/runtime-main.27065dea.js"
+  }
+]);
